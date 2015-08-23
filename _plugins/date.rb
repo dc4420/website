e98f54608deefc96406e2dce20aadc005567d9b6
Mark Steward <marksteward@gmail.com>
2015-08-23 16:45:34 +0000

    Missing plugin

---
+++ _plugins/date.rb
@@ -0,0 +1,15 @@
+module Jekyll
+
+  class EventDateGenerator < Generator
+    safe true
+    def generate(site)
+      site.posts.each do |post|
+        post.data['eventstart'] = post.date + (18 * 60 * 60)
+        post.data['eventend'] = post.date + (23 * 60 * 60)
+      end
+
+    end
+  end
+
+end
+
